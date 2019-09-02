2664a8d70fcb3e04fd999dd041ad1891be2a94f8
Viraj R <virajransing@gmail.com>
2019-09-02 17:12:41 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,22 @@
+class Project
+  
+  attr_reader :backers, :title
+  
+  def initialize(title) 
+    @backers = []
+    @title = title
+  end 
+  
+  def add_backer(backer)
+    @backers << backer
+  end 
+  
+  def add_backer(backer)
+    
+  end 
+  
+  
+  
+  
+  
+end 
